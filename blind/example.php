c12fea837663e2ac5f85584b0ffc1770600b1c15
Muhammad Saleh Solahudin <saleh.solahudin@koinworks.com>
2023-06-22 14:29:47 +0700

    example and how to prevent blind xss

---
+++ blind/example.php
@@ -0,0 +1,36 @@
+<?php
+require_once "../config/db.php";
+session_start();
+
+if (!isset($_SESSION['username'])) {
+  echo "<script>alert('Silahkan login terlebih dahulu!'); location.href = '../auth/login.php';</script>";
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blind XSS</title>
+  </head>
+  <body>
+    <h2>Selamat datang di halaman testimoni produk kami</h2>
+    <form action="" method="POST">
+      <textarea name="testimoni" cols="100" rows="10" placeholder="Silahkan masukkan testimoni anda untuk produk kami yang telah anda konsumsi di sini..." required></textarea>
+      <br>
+      <input type="submit" value="Kirim Testimoni">
+    </form>
+    <?php
+
+      if (isset($_POST['testimoni'])) {
+        $testimoni = $_POST['testimoni'];
+        $sql = "INSERT INTO testimoni VALUES (NULL, '$testimoni')";
+
+        if (mysqli_query($conn, $sql)) echo "<script>alert('Terima kasih untuk testimoni yang telah anda berikan 🤘🏼');</script>";
+        else echo "<script>alert('". mysqli_error($conn) ."')</script>";
+      }
+
+    ?>
+  </body>
+</html>
