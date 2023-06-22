0769d44534bdc5de680a8c246b0efb9f5cabb3b4
Muhammad Saleh Solahudin <saleh.solahudin@koinworks.com>
2023-06-22 14:30:17 +0700

    example and how to prevent self xss

---
+++ self/example.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Self XSS</title>
+  </head>
+  <body>
+    <h2>Ceritanya ini halaman Pesbuk</h2>
+  </body>
+</html>
